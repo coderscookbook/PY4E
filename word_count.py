30d7e7abff9deff5fa3336a9a0754e1c4cdb0dff
Jose A. <soigne@Aria.local>
2024-04-30 13:11:54 -0400

    Add comments to word_count.py

--- word_count.py
+++ word_count.py
@@ -9,18 +9,20 @@ def main():
     # name   = input('Enter file:') # using python3, change to line below
     name   = input('Enter file:')
     handle = open(name)
-    #counts = dict()
-    counts = defaultdict(int)
-    
+    #counts = dict()                                # Method 1
+    counts = defaultdict(int)                       # Method 2
+
     # Create a dictionary of unique words and their count
     for line in handle:
         words = line.split()
         for word in words:
-            #counts[word] = counts.get(word,0) + 1
-            counts[word] += 1
+            #counts[word] = counts.get(word,0) + 1  #Method 1
+            counts[word] += 1                       #Method 2
 
-    print(counts)
-    word_count = None
+    #print(counts)                                  #TESTING ONLY
+    
+    # Find word with most counts and its count
+    # word_count = None
     big_count  = None
     big_word   = None
     for word,count in counts.items():
@@ -28,11 +30,16 @@ def main():
             big_word  = word
             big_count = count
 
-    # print(big_word, big_count)
+    # print(big_word, big_count)                    #TESTING ONLY
     print("Word : ", big_word)
     print("Count: ", big_count)
-
-    
+    #END PROGRAM
 
 if __name__ == "__main__":
+    # Run main code
     main()
+    print("End Program")
+    # Testing timers 
+    #test_code = main()
+    #time_taken = timeit.timeit(stmt=test_code, number=1000)
+    #print("Time to Execute: ", time_taken)
